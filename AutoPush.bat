dadc2082cd546928f88ebfcf741603093ff850a1
Gump-II <332628300@qq.com>
2018-05-19 11:59:00 +0800

    :memo: High Perfomance JavaScript of Notes

--- AutoPush.bat
+++
@@ -1,4 +0,0 @@
-cd ./
-git add .
-git commit -m 'High Perfomance JavaScript of Notes'
-git push -u origin master
---
+++ push.bat
@@ -0,0 +1,4 @@
+cd ./
+git add .
+git commit -m ":memo: High Perfomance JavaScript of Notes"
+git push -u origin master
